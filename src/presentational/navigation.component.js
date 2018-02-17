5738d25af4c26db4cfd15f36e65538b17412454a
Agnieszka Adamczyk <adamcz.aga@gmail.com>
2018-02-17 22:17:07 +0100

    Add search, delete and visibility functionalities

--- src/presentational/navigation.component.js
+++ src/presentational/navigation.component.js
@@ -1,27 +1,33 @@
 import React from "react";
-import {Link} from "react-router";
+import { Link } from "react-router";
 import 'bootstrap/dist/css/bootstrap.css';
-
+import 'bootstrap/dist/css/bootstrap-theme.css';
 
 const Navigation = props => (
 	<div>
 		<nav className="navbar navbar-default">
 			<div className="container-fluid">
 				<div className="navbar-header">
-					<Link className="navbar-brand" to="/">Państwa.js</Link>
+					<Link className="navbar-brand" to="/">
+						Państwa.js
+					</Link>
 				</div>
 				<div className="collapse navbar-collapse">
 					<ul className="nav navbar-nav">
-						<li><Link to="/countries">Countries</Link></li>
-						<li><Link to="/continents">Continents</Link></li>
-						<li><Link to="/contacts">Contacts</Link></li>
+						<li>
+							<Link to="/countries">Countries</Link>
+						</li>
+						<li>
+							<Link to="/continents">Continents</Link>
+						</li>
+						<li>
+							<Link to="/contact">Contacts</Link>
+						</li>
 					</ul>
 				</div>
 			</div>
 		</nav>
-		<div className="container-fluid">
-			{props.children}
-		</div>
+		<div className="container-fluid">{props.children}</div>
 	</div>
 );
 
